8b00bad1a251bc8636f6cd842d15a2720b49d438
Alexander Pilz <pilz@syslab.com>
2012-07-09 17:57:27 +0000

    new
    git-svn-id: https://code.gocept.com/svn/osha/osha.campaigntoolkit/trunk@73282 97d050b9-b799-4550-a901-4f04598d4601

---
+++ osha/campaigntoolkit/browser/toolexample.py
@@ -0,0 +1,16 @@
+from Products.Five.browser import BrowserView
+from zope.interface import implements
+from Products.Archetypes.interfaces.base import IBaseContent, IBaseFolder
+from Products.Archetypes.utils import OrderedDict
+from Products.ATContentTypes.interface.image import IATImage
+from Products.CMFCore.utils import getToolByName
+from DateTime import DateTime
+from Acquisition import aq_parent, aq_inner, aq_base
+from plone.memoize.instance import memoize
+
+
+class ToolExampleView(BrowserView):
+
+
+    def __call__(self):
+        return self.index()
