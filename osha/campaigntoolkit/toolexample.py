06fdf558ef9f6e983a0d4d2137c5dc1e0da5bdb8
Jure Cerjak <jcerjak@termitnjak.si>
2012-09-17 18:47:53 +0000

    Exclude ToolExample objects from navigation
    git-svn-id: https://code.gocept.com/svn/osha/osha.campaigntoolkit/trunk@73916 97d050b9-b799-4550-a901-4f04598d4601

--- osha/campaigntoolkit/toolexample.py
+++ osha/campaigntoolkit/toolexample.py
@@ -4,6 +4,7 @@
 from five import grok
 from osha.campaigntoolkit import  _
 from plone.app.textfield import RichText
+from plone.app.dexterity.behaviors.exclfromnav import IExcludeFromNavigation
 from plone.directives import dexterity, form
 from plone.namedfile.field import NamedBlobImage
 from plone.namedfile.interfaces import IImageScaleTraversable
@@ -32,3 +33,9 @@ class View(grok.View):
     """Default view for the ToolExample"""
     grok.context(IToolExample)
     grok.require('zope2.View')
+
+
+@form.default_value(field=IExcludeFromNavigation['exclude_from_nav'])
+def exclude_from_nav_default(data):
+    """Exclude ToolExample objects from navigation."""
+    return data.request.URL.endswith('++add++osha.campaigntoolkit.toolexample')
