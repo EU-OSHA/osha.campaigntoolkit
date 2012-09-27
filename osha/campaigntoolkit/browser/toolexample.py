dcc048c08713878392d69918e511ea60b2f1fdad
Jure Cerjak <jcerjak@termitnjak.si>
2012-09-27 13:05:54 +0000

    Added a view for the default page of the folder that contains tool examples with a viewlet that shows the latest examples. Updated css and added highlights-carousel.js to jsregistry.
    git-svn-id: https://code.gocept.com/svn/osha/osha.campaigntoolkit/trunk@74042 97d050b9-b799-4550-a901-4f04598d4601

--- osha/campaigntoolkit/browser/toolexample.py
+++ osha/campaigntoolkit/browser/toolexample.py
@@ -1,16 +1,40 @@
+from Acquisition import aq_parent, aq_inner
+from plone.app.layout.viewlets import common as base
+from Products.CMFCore.interfaces import IFolderish
 from Products.Five.browser import BrowserView
-from zope.interface import implements
-from Products.Archetypes.interfaces.base import IBaseContent, IBaseFolder
-from Products.Archetypes.utils import OrderedDict
-from Products.ATContentTypes.interface.image import IATImage
-from Products.CMFCore.utils import getToolByName
-from DateTime import DateTime
-from Acquisition import aq_parent, aq_inner, aq_base
-from plone.memoize.instance import memoize
+
+# Number of latest toolexamples to show in the viewlet
+LIMIT = 4
 
 
 class ToolExampleView(BrowserView):
+    def __call__(self):
+        return self.index()
 
+
+class ToolExamplesView(BrowserView):
+    """View for the default page of the folder that contains tool
+    examples."""
 
     def __call__(self):
         return self.index()
+
+
+class ToolExamplesViewlet(base.ViewletBase):
+    """Viewlet which shows the latest tool examples."""
+
+    def latest_examples(self):
+        if IFolderish.providedBy(self.context):
+            folder = aq_inner(self.context)
+        else:
+            folder = aq_parent(self.context)
+        path = folder.getPhysicalPath()
+        path = "/".join(path)
+
+        return folder.restrictedTraverse('@@folderListing')(
+            path={"query": path, "depth": 1},
+            portal_type='osha.campaigntoolkit.toolexample',
+            review_state='published',
+            sort_on='Date',
+            sort_order='reverse',
+            sort_limit=LIMIT)[:LIMIT]
