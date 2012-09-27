d978273d29eb230972f2527570710155bd4e52bd
Alexander Pilz <pilz@syslab.com>
2012-09-27 10:44:14 +0000

    changed condition to "everything under tools"
    git-svn-id: https://code.gocept.com/svn/osha/osha.campaigntoolkit/trunk@74036 97d050b9-b799-4550-a901-4f04598d4601

--- osha/campaigntoolkit/upgrade.py
+++ osha/campaigntoolkit/upgrade.py
@@ -19,38 +19,37 @@ def migrate_pages_to_toolexample(context):
     """
     portal = getSite()
     catalog = getToolByName(portal, 'portal_catalog')
-    pages = catalog(portal_type='Document')
+    pages = catalog(portal_type='Document', path="/Plone/tools")
 
     logger.info('Starting migration of pages to ToolExample objects...')
 
     for page in pages:
         page = aq_inner(page.getObject())
         text = page.getText()
-        if '<dt>Organisation</dt>' in text:
-            container = aq_parent(page)
-            obj_id = page.getId()
-            tmp_id = obj_id + '.tmp'
-            rich_text = RichTextValue(text.decode('utf-8'), 'text/html',
-                'text/x-html-safe', 'utf-8')
+        container = aq_parent(page)
+        obj_id = page.getId()
+        tmp_id = obj_id + '.tmp'
+        rich_text = RichTextValue(text.decode('utf-8'), 'text/html',
+            'text/x-html-safe', 'utf-8')
 
-            logger.info('Migrating page at %s' % page.absolute_url())
+        logger.info('Migrating page at %s' % page.absolute_url())
 
-            # Create a new toolexample with a temp id
-            container.invokeFactory(
-                type_name='osha.campaigntoolkit.toolexample',
-                title=page.Title(),
-                text=rich_text,
-                id=tmp_id)
+        # Create a new toolexample with a temp id
+        container.invokeFactory(
+            type_name='osha.campaigntoolkit.toolexample',
+            title=page.Title(),
+            text=rich_text,
+            id=tmp_id)
 
-            # Delete the page
-            del container[obj_id]
+        # Delete the page
+        del container[obj_id]
 
-            # This is needed otherwise we get an error when renaming
-            # See http://plone.293351.n2.nabble.com/File-object-migration-how-to-change-id-of-new-object-programatically-2-5-5-td4174543.html
-            transaction.savepoint(1)
+        # This is needed otherwise we get an error when renaming
+        # See http://plone.293351.n2.nabble.com/File-object-migration-how-to-change-id-of-new-object-programatically-2-5-5-td4174543.html
+        transaction.savepoint(1)
 
-            # Rename the toolexample object
-            container.manage_renameObject(tmp_id, obj_id)
+        # Rename the toolexample object
+        container.manage_renameObject(tmp_id, obj_id)
 
     logger.info('Migration done.')
             
