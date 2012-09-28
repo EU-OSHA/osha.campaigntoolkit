e2bcad2dbcd294291e5b4c3e732e772102024ef1
Jure Cerjak <jcerjak@termitnjak.si>
2012-09-28 14:47:39 +0000

    Move ToolExample view to the browser folder, since we have all other views there.
    git-svn-id: https://code.gocept.com/svn/osha/osha.campaigntoolkit/trunk@74074 97d050b9-b799-4550-a901-4f04598d4601

--- osha/campaigntoolkit/toolexample.py
+++ osha/campaigntoolkit/toolexample.py
@@ -1,13 +1,13 @@
 # -*- coding: utf-8 -*-
 """The ToolExample content type."""
 
-from five import grok
 from osha.campaigntoolkit import  _
 from plone.app.textfield import RichText
 from plone.app.dexterity.behaviors.exclfromnav import IExcludeFromNavigation
 from plone.directives import dexterity, form
 from plone.namedfile.field import NamedBlobImage
 from plone.namedfile.interfaces import IImageScaleTraversable
+from zope.interface import implements
 
 
 class IToolExample(form.Schema, IImageScaleTraversable):
@@ -26,13 +26,7 @@ class IToolExample(form.Schema, IImageScaleTraversable):
 
 class ToolExample(dexterity.Container):
     """Example of a campaign tool."""
-    grok.implements(IToolExample)
-
-
-class View(grok.View):
-    """Default view for the ToolExample"""
-    grok.context(IToolExample)
-    grok.require('zope2.View')
+    implements(IToolExample)
 
 
 @form.default_value(field=IExcludeFromNavigation['exclude_from_nav'])
