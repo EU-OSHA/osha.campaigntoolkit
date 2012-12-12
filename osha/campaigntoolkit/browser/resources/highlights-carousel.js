7c686421a07012e87144361102f93ccdceff8708
Peter Lamut <peter.lamut@niteoweb.com>
2012-12-12 14:24:47 +0100

    Tool examples: links to examples now open exampels in an overlay

--- osha/campaigntoolkit/browser/resources/highlights-carousel.js
+++ osha/campaigntoolkit/browser/resources/highlights-carousel.js
@@ -1,3 +1,27 @@
-jQuery(document).ready(function() {
-    jQuery("#highlights-tabs").tabs("#highlights-content > div");
-});
+/*global document, jQuery*/
+
+// initialize links to examples to open in an overlay
+(function ($) {
+    "use strict";
+
+    $(document).ready(function () {
+
+        var overlayOpts = {
+            subtype: "ajax",
+            filter: "#content"
+        };
+
+        $("div.examples a.exampleLink").prepOverlay(overlayOpts);
+    });
+
+}(jQuery));
+
+
+// initialize examples highlights carousel
+(function ($) {
+    "use strict";
+
+    $(document).ready(function () {
+        $("#highlights-tabs").tabs("#highlights-content > div");
+    });
+}(jQuery));
