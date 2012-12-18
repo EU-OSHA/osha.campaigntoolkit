407e8c28951aa7509f96365d6b8686e3afe837e5
Peter Lamut <peter.lamut@niteoweb.com>
2012-12-18 19:02:45 +0100

    Fix #6087: Add upgrade step which disables "exclude from navigation"

---
+++ osha/campaigntoolkit/upgrades/upgrade_001_to_002.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""Upgrade from version 001 to version 002."""
+
+from Products.CMFCore.utils import getToolByName
+
+
+def upgrade(context):
+    """Upgrade steps from 001 to 002."""
+    catalog = getToolByName(context, "portal_catalog")
+
+    brains = catalog(portal_type="osha.campaigntoolkit.toolexample")
+    for b in brains:
+        obj = b.getObject()
+        obj.exclude_from_nav = False
+        obj.reindexObject()
+
+    context.runImportStepFromProfile(
+        'profile-osha.campaigntoolkit:default',
+        'typeinfo'
+    )
