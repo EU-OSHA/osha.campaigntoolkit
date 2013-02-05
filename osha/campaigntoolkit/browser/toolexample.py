EU-OSHA/osha.campaigntoolkit

commit b770ef81822ce1b91e516b3f4aa6d89ac3595d82

Bring back Tool Examples listing, refs #6091.
First, some history: the previous version of the site used a certain content
layout in which you had a Folder with some Tool Examples. This folder also
had a Page and this page was set as the default view for the folder. When
migrating to newer version, all these Pages were transformed into Tool
Examples. This means that the part of the Page template that showed a listing
of Tool Examples needs to be moved to the Tool Example template. This commit
takes care of this.

Apart from that, it adds viewlets.xml to the upgrade step, so you don't need
to manually re-import it in portal_setup.

osha/campaigntoolkit/browser/templates/toolexample.pt
@@ -16,6 +16,26 @@
             <img tal:replace="structure context/@@images/image/mini" />
         </div>
         <div tal:content="structure context/text/output|nothing"></div>
+
+        <div class="examples" tal:condition="view/examples">
+            <h3>Examples</h3>
+            <ul id="examplelist" tal:define="items view/examples"
+                tal:condition="items">
+                <li tal:repeat="item items">
+                    <div class="image-left"
+                         tal:define="item_obj item/getObject"
+                         tal:condition="item_obj/image|nothing">
+                        <img tal:replace="structure item_obj/@@images/image/mini" />
+                    </div>
+                    <h3><a tal:attributes="href item/getURL;
+                                           class string:exampleLink"
+                           tal:content="item/Title">Title</a></h3>
+                    <p tal:content="item/Description">Description</p>
+                </li>
+            </ul>
+            <div class="visualClear"><!-- --></div>
+        </div>
+
     </metal:content-core>
 </metal:content-core>
 

osha/campaigntoolkit/browser/toolexample.py
@@ -1,6 +1,5 @@
 from Acquisition import aq_parent, aq_inner
 from plone.app.layout.viewlets import common as base
-from Products.CMFCore.interfaces import IFolderish
 from Products.Five.browser import BrowserView
 from plone.app.contentlisting.interfaces import IContentListing
 
@@ -13,16 +12,19 @@ def get_examples(context, limit=None, osh_related_only=False):
     parent.
 
     :param context: object where to look for tool examples. If context is
-        not folderish, we look in its parent (useful if we use this method on
-        folder's default view)
+        set as default view for its parent folder, we look in that parent
+        folder
     :param limit: limit the number of results
     :returns: A list of published tool examples, sorted by date, latest first.
     :rtype: ContentListing object
     """
-    if IFolderish.providedBy(context):
-        folder = aq_inner(context)
-    else:
+    if (
+        hasattr(context.__parent__, 'default_page') and
+        context.__parent__.default_page == context.id
+    ):
         folder = aq_parent(context)
+    else:
+        folder = aq_inner(context)
     path = folder.getPhysicalPath()
     path = "/".join(path)
     query = {
@@ -47,13 +49,6 @@ def get_examples(context, limit=None, osh_related_only=False):
 
 class ToolExampleView(BrowserView):
     """View for the ToolExample type."""
-    def __call__(self):
-        return self.index()
-
-
-class ToolExamplesView(BrowserView):
-    """View for the default page of the folder that contains tool
-    examples."""
 
     def __call__(self):
         return self.index()

osha/campaigntoolkit/tests/test_views.py
@@ -76,13 +76,16 @@ class TestToolExampleViews(IntegrationTestCase):
         examples = get_examples(self.portal.folder)
         self.assertEqual(len(examples), 3)
 
-    def test_get_examples_non_folderish_context(self):
+    def test_get_examples_on_a_default_layout_item(self):
         """Test get_examples method for a non-folderish context."""
         from osha.campaigntoolkit.browser.toolexample import get_examples
 
-        # As the context use page contained in the folder with tool
-        # examples (to simulate usage of this method on a default folder view)
-        examples = get_examples(self.portal.folder['page1'])
+        examples = get_examples(self.portal.folder['example1'])
+        self.assertEqual(len(examples), 0)
+
+        # now set example1 to be default view for folder
+        self.portal['folder'].default_page = 'example1'
+        examples = get_examples(self.portal.folder['example1'])
         self.assertEqual(len(examples), 3)
 
 

osha/campaigntoolkit/browser/configure.zcml
@@ -48,25 +48,6 @@
         description="A two column overview"
         />
 
-    <!-- view for the default page of the folder that contains tool
-         examples. -->
-    <browser:page
-        for="Products.ATContentTypes.interfaces.document.IATDocument"
-        name="toolexamples"
-        class=".toolexample.ToolExamplesView"
-        template="templates/toolexamples.pt"
-        permission="zope2.View"
-       />
-
-    <browser:menuItem
-        for="Products.ATContentTypes.interfaces.document.IATDocument"
-        menu="plone_displayviews"
-        title="Tool Examples view"
-        action="@@toolexamples"
-        description="View for the default page of the folder that contains
-                     tool examples."
-        />
-
     <!-- viewlets -->
     <browser:viewlet
         name="osha.campaigntoolkit.toolexamples_viewlet"

osha/campaigntoolkit/upgrades/upgrade_001_to_002.py
@@ -23,3 +23,8 @@ def upgrade(context):
         'profile-osha.campaigntoolkit:default',
         'catalog'
     )
+
+    context.runImportStepFromProfile(
+        'profile-osha.campaigntoolkit:default',
+        'viewlets'
+    )

commit 48b082eeb4d7808e0b4c25e1c0a42910857184e1

fix the toolexample view, refs #6091, #6092, #6562
Commit b770ef81822ce1b91e516b3f4aa6d89ac3595d82 broke some of the changes done in #6092. This commit brings back the styling for toolexample view defined in that ticket (with examples box on the right etc.).

Additionally it fixes:
- don't display the tool example which is set as the default view in the related tool examples box,
- don't display related tool examples box on the example pop-up view
- fix image display in the example box
- delete toolexamples.pt because it is not used anymore
- add some documentation in ToolExample view, so we
don't get confused the next time :)

osha/campaigntoolkit/browser/resources/campaigntoolkit.css
@@ -754,6 +754,7 @@ div.examples > h3 {
     float: left;
     margin: 0 1em 1em 0;
     width: 80px;
+    height: auto;
     clear: both;
 }
 #examplelist li:first-child {

osha/campaigntoolkit/browser/templates/toolexample.pt
@@ -9,31 +9,38 @@
 
 <metal:content-core fill-slot="content-core">
     <metal:content-core define-macro="content-core">
-        <a class="link-parent"
-           tal:attributes="href string:${context/aq_inner/aq_parent/absolute_url}"
-           i18n:translate="">Back to Overview</a>
-        <div class="image-right" tal:condition="context/image|nothing">
-            <img tal:replace="structure context/@@images/image/mini" />
-        </div>
-        <div tal:content="structure context/text/output|nothing"></div>
 
-        <div class="examples" tal:condition="view/examples">
-            <h3>Examples</h3>
-            <ul id="examplelist" tal:define="items view/examples"
-                tal:condition="items">
-                <li tal:repeat="item items">
-                    <div class="image-left"
-                         tal:define="item_obj item/getObject"
-                         tal:condition="item_obj/image|nothing">
-                        <img tal:replace="structure item_obj/@@images/image/mini" />
-                    </div>
-                    <h3><a tal:attributes="href item/getURL;
-                                           class string:exampleLink"
-                           tal:content="item/Title">Title</a></h3>
-                    <p tal:content="item/Description">Description</p>
-                </li>
-            </ul>
-            <div class="visualClear"><!-- --></div>
+        <div class="panes"
+             tal:define="examples view/examples;
+                         has_image context/image|nothing;">
+            <div class="overview">
+                <div tal:content="structure context/text/output|nothing"></div>
+            </div>
+
+            <div class="image-right" tal:condition="python: not examples and has_image">
+              <img tal:replace="structure context/@@images/image/mini" />
+            </div>
+
+            <div class="examples" tal:condition="examples">
+                <h3>Examples</h3>
+                <ul id="examplelist">
+                    <li tal:repeat="item examples">
+                        <div class="image-left"
+                             tal:define="item_obj item/getObject"
+                             tal:condition="item_obj/image|nothing">
+                          <a tal:attributes="href item/getURL;
+                                             class string:exampleLink">
+                            <img tal:replace="structure item_obj/@@images/image/thumb" />
+                          </a>
+                        </div>
+                        <h3><a tal:attributes="href item/getURL;
+                                       class string:exampleLink"
+                               tal:content="item/Title">Title</a></h3>
+                        <p tal:content="item/Description">Description</p>
+                    </li>
+                </ul>
+                <div class="visualClear"><!-- --></div>
+            </div>
         </div>
 
     </metal:content-core>

osha/campaigntoolkit/browser/templates/toolexamples.pt
@@ -1,67 +1,0 @@
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en"
-      lang="en"
-      metal:use-macro="here/main_template/macros/master"
-      i18n:domain="osha">
-
-  <metal:javascript fill-slot="javascript_head_slot">
-      <script type="text/javascript" language="javascript">
-          $(function() {
-              // setup ul.tabs to work as tabs for each div directly under
-              // div.panes
-              $("ul.tabs").tabs("div.panes > div");
-
-              // Hide the tool example highlights on the 'Examples' tab
-              $("a#examples").click(function(){
-                  $("#highlightsContainer").hide();
-              });
-              $("a#overview").click(function(){
-                  $("#highlightsContainer").show();
-              });
-        });
-    </script>
-  </metal:javascript>
-  <metal:css fill-slot="css_slot"></metal:css>
-
-<body>
-
-<div metal:fill-slot="main">
-    <div id="mainContent">
-        <h1 tal:content="here/Title"></h1>
-        <p class="documentDescription" tal:content="here/Description" />
-
-        <div class="panes">
-
-            <div class="overview">
-                <div tal:content="structure here/getText">Body text</div>
-            </div>
-
-
-            <div class="examples">
-                <h3>Examples</h3>
-                <ul id="examplelist" tal:define="items view/examples"
-                    tal:condition="items">
-                    <li tal:repeat="item items">
-                        <div class="image-left"
-                             tal:define="item_obj item/getObject"
-                             tal:condition="item_obj/image|nothing">
-                            <img tal:replace="structure item_obj/@@images/image/mini" />
-                        </div>
-                        <h3><a tal:attributes="href item/getURL;
-                                               class string:exampleLink"
-                               tal:content="item/Title">Title</a></h3>
-                        <p tal:content="item/Description">Description</p>
-                    </li>
-                </ul>
-                <div class="visualClear"><!-- --></div>
-            </div>
-
-            <div class="visualClear"><!-- --></div>
-
-        </div>
-    </div>
-</div>
-
-
-</body>
-
-</html>

osha/campaigntoolkit/browser/toolexample.py
@@ -1,4 +1,4 @@
-from Acquisition import aq_parent, aq_inner
+from Acquisition import aq_parent
 from plone.app.layout.viewlets import common as base
 from Products.Five.browser import BrowserView
 from plone.app.contentlisting.interfaces import IContentListing
@@ -8,24 +8,14 @@ LIMIT = 4
 
 
 def get_examples(context, limit=None, osh_related_only=False):
-    """Return ToolExample objects contained in the provided context or its
-    parent.
+    """Return ToolExample objects contained in the provided context.
 
-    :param context: object where to look for tool examples. If context is
-        set as default view for its parent folder, we look in that parent
-        folder
+    :param context: object where to look for tool examples.
     :param limit: limit the number of results
     :returns: A list of published tool examples, sorted by date, latest first.
     :rtype: ContentListing object
     """
-    if (
-        hasattr(context.__parent__, 'default_page') and
-        context.__parent__.default_page == context.id
-    ):
-        folder = aq_parent(context)
-    else:
-        folder = aq_inner(context)
-    path = folder.getPhysicalPath()
+    path = context.getPhysicalPath()
     path = "/".join(path)
     query = {
         'path': {"query": path},
@@ -37,40 +27,76 @@ def get_examples(context, limit=None, osh_related_only=False):
     if osh_related_only:
         query['OSH_related'] = True
 
-    if folder == context.portal_url.getPortalObject():
+    if context == context.portal_url.getPortalObject():
         return IContentListing(context.portal_catalog(query)[:limit])
 
     if limit is not None:
         query['sort_limit'] = limit
-        return folder.restrictedTraverse('@@folderListing')(**query)[:limit]
+        return context.restrictedTraverse('@@folderListing')(**query)[:limit]
     else:
-        return folder.restrictedTraverse('@@folderListing')(**query)
+        return context.restrictedTraverse('@@folderListing')(**query)
 
 
 class ToolExampleView(BrowserView):
-    """View for the ToolExample type."""
+    """View for the ToolExample type.
+
+    Note that ToolExample type and the accompanying view are used in two
+    related, but different scenarios:
+     - to show general information about a Tool Example (e.g. 'Poster') with
+       a list of practical examples
+     - to show a practical example (e.g. 'Tips for the safe use of gloves')
+
+    We have the following structure:
+
+        Folder with Tool Examples -> toolexample1 as default view
+          |
+          |--toolexample1 -> ToolExampleView (tool example overview with a
+                                              list of practical examples)
+          |--toolexample2 -> ToolExampleView (practical example view)
+          |--toolexample3 -> ToolExampleView (practical example view)
+          |..
+
+    """
 
     def __call__(self):
         return self.index()
 
     def examples(self):
-        """Return tool examples contained in this folder.
+        """Return tool examples contained in the parent folder.
+
+        Search for results only if context is set as default view on the
+        parent folder, otherwise return an empty list.
 
         OSH related examples have sorting precedence over the others.
         """
-        examples = get_examples(self.context)
+        if not self._context_is_default_view():
+            return []
 
         # OSH related examples should come first
         # (NOTE: items in list are already sorted by date)
         osh_related, others = [], []
 
-        for item in examples:
+        for item in get_examples(aq_parent(self.context)):
+            # don't include the context
+            if item.id == self.context.id:
+                continue
             if item.OSH_related:
                 osh_related.append(item)
             else:
                 others.append(item)
 
         return osh_related + others
+
+    def _context_is_default_view(self):
+        """Check if context is set as default view on the parent folder.
+        """
+        if (
+            hasattr(self.context.__parent__, 'default_page') and
+            self.context.__parent__.default_page == self.context.id
+        ):
+            return True
+        else:
+            return False
 
 
 class ToolExamplesViewlet(base.ViewletBase):

osha/campaigntoolkit/tests/test_views.py
@@ -69,24 +69,28 @@ class TestToolExampleViews(IntegrationTestCase):
         self.assertEqual(examples[0].getId(), 'example3')
         self.assertEqual(examples[1].getId(), 'example2')
 
-    def test_get_examples_folderish_context(self):
-        """Test get_examples method for a folderish context."""
-        from osha.campaigntoolkit.browser.toolexample import get_examples
+    def test_examples_on_a_default_layout_item(self):
+        """Test examples method of the ToolExampleView for a non-folderish
+        context.
+        """
 
-        examples = get_examples(self.portal.folder)
-        self.assertEqual(len(examples), 3)
+        example2 = self.portal.folder['example2']
+        view = example2.restrictedTraverse('@@toolexample-view')
 
-    def test_get_examples_on_a_default_layout_item(self):
-        """Test get_examples method for a non-folderish context."""
-        from osha.campaigntoolkit.browser.toolexample import get_examples
-
-        examples = get_examples(self.portal.folder['example1'])
+        # if tool example is not set as default view, we should get an empty
+        # list
+        examples = view.examples()
         self.assertEqual(len(examples), 0)
 
-        # now set example1 to be default view for folder
-        self.portal['folder'].default_page = 'example1'
-        examples = get_examples(self.portal.folder['example1'])
-        self.assertEqual(len(examples), 3)
+        # now set example2 to be default view for folder
+        self.portal['folder'].default_page = 'example2'
+        examples = view.examples()
+
+        # example2 should be missing from the results, because it is set
+        # as default view
+        self.assertEqual(len(examples), 2)
+        self.assertEqual(examples[0].getId(), 'example1')
+        self.assertEqual(examples[1].getId(), 'example3')
 
 
 def test_suite():